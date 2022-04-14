a0c694c06b55ea9940db1b5a66dedd9f96d3e232
LiFeng <3036944615@qq.com>
2022-04-14 19:00:55 +0800

    版本4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("版本2");
         System.out.println("版本3");
+        System.out.println("hot 4");
     }
 }
