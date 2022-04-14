bbeced9395b02a474ff69166b3d118a7b12efcc1
LiFeng <3036944615@qq.com>
2022-04-14 18:51:55 +0800

    版本2

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 付剑南
+ * @create 2022-04-14 18:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("版本2");
+    }
+}
