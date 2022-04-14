fd5bc87f134294bf927f755aab6ccffcc0aaefe1
LiFeng <3036944615@qq.com>
2022-04-14 20:37:51 +0800

    hot1 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -23,6 +23,7 @@ public class GitTest {
         System.out.println("本地库不提交，能否拉取远程库更新");
         System.out.println("测试远程库有更新，本地库有修改但是没提交，能否拉取，测试可以直接拉取，但是要合并，并且本地库不会生成版本信息");
         System.out.println("hot1 1");
+        System.out.println("hot1 2");
 
     }
 }
